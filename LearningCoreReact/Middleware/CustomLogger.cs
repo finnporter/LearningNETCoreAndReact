5ba2b8f4c181e7255dca22fb626f1d7f0779499c
Finn Porter <iamfinnporter@gmail.com>
2020-07-21 12:14:36 +0100

    Add project files.

---
+++ LearningCoreReact/Middleware/CustomLogger.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningCoreReact.Middleware
+{
+    public class CustomLogger
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomLogger (RequestDelegate next)
+        {
+            _next = next ?? throw new ArgumentNullException(nameof(next));
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if (httpContext == null)
+            {
+                throw new ArgumentNullException(nameof(httpContext));                
+            }
+
+            //TODO log request
+            Console.WriteLine(httpContext.Request.Body);
+
+            await _next(httpContext);
+
+            //TODO - log resonse
+            Console.WriteLine(httpContext.Response.Body);
+        }
+    }
+}
---
+++ LearningCoreReact/Middleware/MiddlewareExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningCoreReact.Middleware
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomLogger(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<CustomLogger>();
+        }
+    }
+}
